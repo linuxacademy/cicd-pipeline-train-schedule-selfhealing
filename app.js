2aa587846f9657cfb8afbb9a820f79334f7d621e
willb <willb@linuxacademy.com>
2018-05-22 16:11:11 -0400

    add endpoint to simulate unhealthy app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var broken = false;
 
 var indexRouter = require('./routes/index');
 var trainsRouter = require('./routes/trains');
@@ -19,8 +20,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+   res.locals = {
+     broken: broken
+   };
+   next();
+});
+
 app.use('/', indexRouter);
 app.use('/trains', trainsRouter);
+
+//this endpoint triggers the app to simulate entering an unhealthy state by causing it to return 5XX errors.
+app.get('/break', function(req, res, next) {
+	broken = true;
+	res.status(200).send('The app is now broken!')
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if (!res.locals.broken) {
+    res.render('index', { title: 'Express' });
+  } else {
+  	res.status(500).send('Something broke!');
+  }
 });
 
 module.exports = router;
